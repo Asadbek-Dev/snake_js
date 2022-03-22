f613786ddefaa00f6581e40406a07c5c297156db
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-03-23 00:01:47 +0500

    added style

--- main.js
+++ main.js
@@ -13,30 +13,52 @@ class Snake {
     this.rotateY = 1;
   }
   move() {
-      var newRect;
-      if(this.rotateX==1){
-          newRect={
-              x:this.tail[this.tail.length-1].x+this.size,
-              y:this.tail[this.tail.length-1].y,
-          }
-      } else if(this.rotateX==-1){
-        newRect={
-            x:this.tail[this.tail.length-1].x-this.size,
-            y:this.tail[this.tail.length-1].y,
-        }
-    } else if(this.rotateY==1){
-        newRect={
-            x:this.tail[this.tail.length-1].x,
-            y:this.tail[this.tail.length-1].y+this.size,
-        }
-    } else if(this.rotateY==-1){
-        newRect={
-            x:this.tail[this.tail.length-1].x,
-            y:this.tail[this.tail.length-1].y-this.size,
-        }
+    var newRect;
+    if (this.rotateX == 1) {
+      newRect = {
+        x: this.tail[this.tail.length - 1].x + this.size,
+        y: this.tail[this.tail.length - 1].y,
+      };
+    } else if (this.rotateX == -1) {
+      newRect = {
+        x: this.tail[this.tail.length - 1].x - this.size,
+        y: this.tail[this.tail.length - 1].y,
+      };
+    } else if (this.rotateY == 1) {
+      newRect = {
+        x: this.tail[this.tail.length - 1].x,
+        y: this.tail[this.tail.length - 1].y + this.size,
+      };
+    } else if (this.rotateY == -1) {
+      newRect = {
+        x: this.tail[this.tail.length - 1].x,
+        y: this.tail[this.tail.length - 1].y - this.size,
+      };
     }
     this.tail.shift();
-    this.tail.push(newRect)
+    this.tail.push(newRect);
+  }
+}
+class Apple {
+  constructor() {
+    var isTouching;
+    while (true) {
+      isTouching = false;
+      this.x =
+        Math.floor((Math.random() * canvas.width) / snake.size) * snake.size;
+      this.y =
+        Math.floor((Math.random() * canvas.height) / snake.size) * snake.size;
+      for (var i = 0; i < snake.tail.length; i++) {
+        if (this.x == snake.tail[i].x && this.y == snake.tail[i].y) {
+          isTouching = true;
+        }
+      }
+      if (!isTouching) {
+        break;
+      }
+      this.color='pink'
+      this.size=snake.size
+    }
   }
 }
 
@@ -56,4 +78,29 @@ function gameLoop() {
 function show() {
   update();
   draw();
+}
+
+function update(){
+
+}
+
+
+function draw(){
+    createRect(0,0,canvas.width,canvas.height,'black')
+    createRect(0,0,canvas.width,canvas.height)
+    for (let i = 0; i < snake.tail.length; i++) {
+        createRect(snake.tail[i].x+2.5,snake.tail[i].y+2.5,
+            snake.size-5,snake.size-5,'white')
+        
+    }
+
+    canvasContext.front='20px Arial'
+    canvasContext.fillStyle='#DDFF42'
+    canvasContext.fillText("Score: ",(snake.tail.length+1),canvas.width-120,18);
+    createRect(apple.x,apple.y,apple.size,apple.size,apple.color)
+    
+}
+function createRect(x,y,width,height,color){
+    canvasContext.fillStyle=color
+    canvasContext.fillRect(x,y,width,height)
 }
