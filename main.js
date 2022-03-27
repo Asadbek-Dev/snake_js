a801b16064ad2442451d713e89d5ed92c83df382
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-03-27 21:43:26 +0500

    fixed

--- main.js
+++ main.js
@@ -56,14 +56,14 @@ class Apple {
       if (!isTouching) {
         break;
       }
-      this.color='pink'
-      this.size=snake.size
+      this.color = "pink";
+      this.size = snake.size;
     }
   }
 }
 
 var canvas = document.getElementById("canvas");
-var snake = new Snake(20,20,20);
+var snake = new Snake(20, 20, 20);
 var apple = new Apple();
 var canvasContext = canvas.getContext("2d");
 
@@ -80,46 +80,61 @@ function show() {
   draw();
 }
 
-function update(){
-  canvasContext.clearRect(0,0,canvas.width,canvas.height)
-  snake.move()
+function update() {
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  snake.move();
+  eatApple();
 }
 
+function eatApple(){
+  if(snake.tail[snake.tail.length-1].x == apple.x && snake.tail[snake.tail.length-1].y == apple.y){
+    snake.tail[snake.tail.length] = {x:apple.x,y:apple.y}
+    apple=new Apple();
+  }
+}
 
-function draw(){
-    createRect(0,0,canvas.width,canvas.height,'black')
-    createRect(0,0,canvas.width,canvas.height)
-    for (let i = 0; i < snake.tail.length; i++) {
-        createRect(snake.tail[i].x+2.5,snake.tail[i].y+2.5,
-            snake.size-5,snake.size-5,'white')
-        
+function draw() {
+  createRect(0, 0, canvas.width, canvas.height, "black");
+  createRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < snake.tail.length; i++) {
+    createRect(
+      snake.tail[i].x + 2.5,
+      snake.tail[i].y + 2.5,
+      snake.size - 5,
+      snake.size - 5,
+      "white"
+    );
+  }
+
+  canvasContext.front = "20px Arial";
+  canvasContext.fillStyle = "#DDFF42";
+  canvasContext.fillText(
+    "Score: "+
+    (snake.tail.length - 1),
+    canvas.width - 120,
+    18
+  );
+  createRect(apple.x, apple.y, 20, 20, 'pink');
+}
+function createRect(x, y, width, height, color) {
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(x, y, width, height);
+}
+
+window.addEventListener("keydown", (event) => {
+  setTimeout(() => {
+    if (event.keyCode == 37 && snake.rotateX != 1) {
+      snake.rotateX = -1;
+      snake.rotateY = 0;
+    } else if (event.keyCode == 38 && snake.rotateY != 1) {
+      snake.rotateX = 0;
+      snake.rotateY = -1;
+    } else if (event.keyCode == 39 && snake.rotateX != -1) {
+      snake.rotateX = 1;
+      snake.rotateY = 0;
+    } else if (event.keyCode == 40 && snake.rotateY != -1) {
+      snake.rotateX = 0;
+      snake.rotateY = 1;
     }
-
-    canvasContext.front='20px Arial'
-    canvasContext.fillStyle='#DDFF42'
-    canvasContext.fillText("Score: ",(snake.tail.length+1),canvas.width-120,18);
-    createRect(apple.x,apple.y,apple.size,apple.size,apple.color)
-    
-}
-function createRect(x,y,width,height,color){
-    canvasContext.fillStyle=color
-    canvasContext.fillRect(x,y,width,height)
-}
-
-window.addEventListener('keydown',(event)=>{
-  setTimeout(()=>{
-    if(event.keyCode==37 && snake.rotateX !=1){
-      snake.rotateX=-1
-      snake.rotateY=0
-    } else if(event.keyCode==38 && snake.rotateY !=1){
-      snake.rotateX=0
-      snake.rotateY=-1
-    }  else if(event.keyCode==39 && snake.rotateX !=-1){
-      snake.rotateX=1
-      snake.rotateY=0
-    } else if(event.keyCode==40 && snake.rotateY !=-1){
-      snake.rotateX=0
-      snake.rotateY=1
-    }
-  })
-})
+  });
+});
